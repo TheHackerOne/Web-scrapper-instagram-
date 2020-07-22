1460a3c99df50b1c057e790c3f12763df8a95dec
TheHackerOne <yash2200@gmail.com>
2020-07-22 18:41:09 +0530

    updated

--- server.js
+++ server.js
@@ -11,22 +11,21 @@ const PORT = process.env.PORT || 5500;
 
 let postCount, imgURL, followers;
 
-setInterval(() => {
-  ig.scrapeUserPage("advertere.tms").then((result) => {
-    postCount = result.user.edge_owner_to_timeline_media.count;
-    followers = result.user.edge_followed_by.count;
-		imgURL = result.user.edge_owner_to_timeline_media.edges.map(
-			(edge) => {
-				return edge.node.thumbnail_resources[0].src;
-			}
-		);
-		// console.log(postCount, imgURL)
-	});
-}, 120000);
+
 
 
 app.get("/", (req, res, next) => {
-  res.send({ postCount: postCount, followers: followers, imgURL: imgURL });	
+  setInterval(() => {
+		ig.scrapeUserPage("advertere.tms").then((result) => {
+			postCount = result.user.edge_owner_to_timeline_media.count;
+			followers = result.user.edge_followed_by.count;
+			imgURL = result.user.edge_owner_to_timeline_media.edges.map((edge) => {
+				return edge.node.thumbnail_resources[0].src;
+			});
+			// console.log(postCount, imgURL)
+		});
+    res.send({ postCount: postCount, followers: followers, imgURL: imgURL });	
+	}, 120000);
 });
 
 app.listen(PORT, () => {
